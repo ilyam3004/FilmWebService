dfbc691df5e965507230bd49950bd58f43765cc4
ilyam3004 <ilyamelnichuk3004@gmail.com>
2022-05-14 23:55:44 +0300

    athentification-Requests

--- src/pages/authentication/SignIn.jsx
+++ src/pages/authentication/SignIn.jsx
@@ -2,39 +2,58 @@ import React from 'react'
 import { useState } from 'react'
 import '../../App.css';
 import './Authentication.css';
-import axios from 'axios';
 import Input from '../../components/UI/input/Input.jsx'
 import Button from '../../components/UI/button/Button';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
+  const [data, setData] = useState({  login: '', password: '' });
+  const url = 'https://localhost:5001/api/login';
+  let navigate = useNavigate();
 
-  const [password, setPassword] = useState('');
-
-  const [login, setLogin] = useState('');
-
-  const [token, setToken] = useState('');
-
-  async function Login(){
-        const response = axios.post("https://localhost:5001/api/login", {
-            login: login,
-            password: password
-        }).then((response) => {
-            setToken(response.data.token);
-            console.log(token);
-        });
+  async function Login(e){ 
+      e.preventDefault();
+      const requestData = { login: data.login, password: data.password};
+      console.log(JSON.stringify(requestData));
+      await fetch(url, {
+        method: 'POST',
+        headers: {
+           'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+      }).then((result) => {
+          if(result.status === 400){
+            console.log('invalid data')
+          }else if(result.status === 200){
+            navigate("../watched", { replace: true });
+          }
+      });
   }
-  
+  const onChange = (e) => {
+    e.persist();
+    console.log(data);
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
   return (
-      <body>
+      <div className='main__container'>
         <div className='sign__in__container'>
           <h2 className='welcome__text'>Welcome</h2>
           <div className="sign__in__input__container">
-            <Input placeholder='Email' type='text'/>
-            <Input placeholder='Password' type='password'/>  
+            <Input 
+                placeholder='Email' 
+                name='login' 
+                value={data.login}
+                onChange={onChange}/>
+            <Input 
+                placeholder='Password' 
+                type='password'
+                name='password'
+                value={data.password}
+                onChange={onChange}/>  
           </div>
           <div className="btn__container">
-            <Button>Login</Button>
+            <Button onClick={Login}>Login</Button>
           </div>
           <div className="link__container">
             <div className="answer__text">Don't have an account?{' '}
@@ -44,7 +63,7 @@ const SignIn = () => {
             </div>
           </div>
         </div>
-      </body>
+      </div>
   )
 }
 
--- src/pages/authentication/SignUp.jsx
+++ src/pages/authentication/SignUp.jsx
@@ -1,31 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../../components/UI/input/Input';
 import Button from '../../components/UI/button/Button';
 import '../authentication/Authentication.css';
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [data, setData] = useState({  login: '', password: '' });
+  const url = 'https://localhost:5001/api/register';
+  let navigate = useNavigate();
+
+  async function Registration (e){
+    e.preventDefault();
+    const requestData = { login: data.login, password: data.password};
+    console.log(JSON.stringify(requestData));
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestData)
+    }).then((result) => {
+        if(result.status === 400){
+          console.log('user with the same email already exists')
+        }else if(result.status === 200){
+          navigate("../signin", { replace: true });
+        }
+    });
+  }
+  const onChange = (e) => {
+    e.persist();
+    console.log(data);
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
+
   return (
-    <body>
-        <div className='sign__up__container'>
-          <h2 className='welcome__text'>Create account</h2>
-          <div className="sign__up__input__container">
-            <Input placeholder='Email' type='text'/>
-            <Input placeholder='Password' type='password'/>
-            <Input placeholder='Confirm Password' type='password'/>    
-          </div>
-          <div className="btn__container">
-            <Button>Sign up</Button>
-          </div>
-          <div className="link__container">
-            <div className="answer__text">Have an account?{' '}
-              <Link to='/signin' className='link'>
-                SignIn
-              </Link>
+    <form onSubmit={Registration}>
+      <div className='main__container'>
+          <div className='sign__up__container'>
+            <h2 className='welcome__text'>Create account</h2>
+            <div className="sign__up__input__container">
+              <Input placeholder='Email' 
+                  type='text' 
+                  name="login" 
+                  onChange={onChange} 
+                  value={data.login}/>
+              <Input placeholder='Password' 
+                  type='password'
+                  name='password'
+                  onChange={onChange}
+                  value={data.password}/>
+              <Input placeholder='Confirm Password' type='password'/>    
+            </div>
+            <div className="btn__container">
+              <Button>Sign up</Button>
+            </div>
+            <div className="link__container">
+              <div className="answer__text">Have an account?{' '}
+                <Link to='/signin' className='link'>
+                  SignIn
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </body>
+      </form>
   )
 }
 
